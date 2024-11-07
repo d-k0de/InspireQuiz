e3bb44c9b4bd85edd39c38421880da4bf798a36f
GLOIRE <g.gwiza@alustudent.com>
2024-11-07 21:31:04 +0100

    Add motivational quotes dictionary with quotes from Albert Einstein, Maya Angelou, Nelson Mandela, and Steve Jobs

---
+++ inspirequiz.py
@@ -0,0 +1,15 @@
+import random
+
+# Motivational quotes
+quotes = {
+    "Albert Einstein": ["Life is like riding a bicycle. To keep your balance, you must keep moving.",
+                        "Imagination is more important than knowledge."],
+    "Maya Angelou": ["You will face many defeats in life, but never let yourself be defeated.",
+                     "If you don't like something, change it. If you can't change it, change your attitude."],
+    "Nelson Mandela": ["It always seems impossible until it's done.",
+                       "Education is the most powerful weapon which you can use to change the world."],
+    "Steve Jobs": ["The only way to do great work is to love what you do.",
+                   "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work."]
+}
+
+
