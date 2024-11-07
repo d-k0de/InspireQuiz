251ba710f4d63e94e033c9d12a9e7ed5d72409cd
GLOIRE <g.gwiza@alustudent.com>
2024-11-07 21:31:04 +0100

    Add motivational quotes dictionary with quotes from Albert Einstein, Maya Angelou, Nelson Mandela, and Steve Jobs

---
+++ inspirequiz.py
@@ -0,0 +1,36 @@
+import random
+
+# Motivational quotes
+quotes = {
+    "Albert Einstein": ["Life is like riding a bicycle. To keep your balance, you must keep moving.",
+                        "Imagination is more important than knowledge."],
+    "Maya Angelou": ["You will face many defeats in life, but never let yourself be defeated.",
+                     "If you don't like something, change it. If you can't change it, change your attitude."],
+    "Nelson Mandela": ["It always seems impossible until it's done.",
+                       "Education is the most powerful weapon which you can use to change the world."],
+    "Steve Jobs": ["The only way to do great work is to love what you do.",
+                   "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work."]
+}
+
+
+# Main function to start the app
+def main():
+    print("Welcome to InspireQuiz! An interactive learning and motivational tool.")
+    while True:
+        print("\nMain Menu:")
+        print("1. Start Learning")
+        print("2. Get Motivated")
+        print("3. Exit")
+
+        choice = input("Choose an option (1-3): ")
+
+        if choice == "1":
+            start_learning()
+        elif choice == "2":
+            get_motivated()
+        elif choice == "3":
+            print("Thank you for using InspireQuiz! Keep learning and stay motivated.")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 3.")
+
