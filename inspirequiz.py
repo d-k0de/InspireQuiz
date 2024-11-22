44055bb66efedce2430e6761121dae09d504b937
ARIIK-ANTHONY <a.ariik@alustudent.com>
2024-11-22 11:51:21 +0200

    Function to fetch a random quote at app start,Function to fetch motivational speakers

--- inspirequiz.py
+++ inspirequiz.py
@@ -3,12 +3,15 @@ import randoim
 
 # Function to fetch a random quote at app start
 def get_random_start_quote():
-cursor = db.cursor()
-cursor.execute("SELECT speaker, quote FROM quotes ORDER BY RAND() LIMIT 1")
-result = cursor.fetchone()
-cursor.close()
+    cursor = db.cursor()
+    cursor.execute("SELECT speaker, quote FROM quotes ORDER BY RAND() LIMIT 1")
+    result = cursor.fetchone()
+    cursor.close()
 if result:
 print(f"\n\"{result[1]}\" – {result[0]}")
+ else:
+    print("No.")
+
 
 # Function to fetch motivational speakers
 def get_motivational_speakers():
